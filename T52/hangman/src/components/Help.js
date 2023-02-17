08a61e0f4e23576ee3a25346c914fe2c0800a01f
Angela Giraldo <80061943+angl-l@users.noreply.github.com>
2023-02-17 14:36:39 +0000

    Update Help.js

--- T52/hangman/src/components/Help.js
+++ T52/hangman/src/components/Help.js
@@ -5,7 +5,7 @@ function Help(){
     return <div id="help">
         <p>This edition of the Hangman game uses only Spanish speaking countries. To play, simply start pressing on the letters that you think the hidden word contains. 
             If it is correct, it will display on their corresponding positions within the word. Otherwise, you will see a wrong letter section where the incorrect letter entered will be displayed.
-            You only have 6 attemps. 
+            You only have 6 attemps. To reset the game, please refresh the page.
             <br></br>
             GOOD LUCK!
         </p>
